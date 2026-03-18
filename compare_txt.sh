44d9ffeffc7880eed249d1d9cd226eccdce046fd
Cyprien Vinson <Cyprien.Vinson@etu.univ-grenoble-alpes.fr>
2026-03-18 15:26:20 +0100

    compare_txt.sh now use 1 arg
    ne prend qu'un argument en entrée puisqu'il va comparer si ce fichier est bien le meme dans server/ et client/

--- compare_txt.sh
+++ compare_txt.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 # Vérification du nombre d'arguments
-if [ "$#" -ne 2 ]; then
-    echo "Usage : $0 fichier1.txt fichier2.txt"
+if [ "$#" -ne 1 ]; then
+    echo "To compare files with same name from directory server and client"
+    echo "Usage : $0 nomFic"
     exit 1
 fi
 
-FILE1="$1"
-FILE2="$2"
+FILE1=server/"$1"
+FILE2=client/"$1"
 
 # Vérification que les fichiers existent
 if [ ! -f "$FILE1" ] || [ ! -f "$FILE2" ]; then
@@ -17,7 +18,7 @@ fi
 
 # Comparaison des fichiers
 if diff -q "$FILE1" "$FILE2" > /dev/null; then
-    echo "Les fichiers sont identiques : c'est bon."
+    echo "Les fichiers $FILE1 $FILE2 sont identiques : c'est bon."
 else
     echo "Les fichiers sont différents. Détails des différences :"
     diff "$FILE1" "$FILE2"
